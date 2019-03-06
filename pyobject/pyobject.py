a9244898934c8b9073498f3b8671036e2f2764df
zhouxianggen <zhouxianggen@gmail.com>
2019-03-06 09:49:35 +0800

    update

--- pyobject/pyobject.py
+++ pyobject/pyobject.py
@@ -1,17 +1,22 @@
 # coding: utf8 
 import logging
+from logging.handlers import RotatingFileHandler
 from datetime import datetime
 
 
 class PyObject(object):
-    def __init__(self, logLevel=logging.INFO):
+    def __init__(self, logLevel=logging.INFO, logPath=''):
         self.log = logging.getLogger(self.__class__.__name__)
         self.log.setLevel(logLevel)
         self.log.propagate = False
         if not self.log.handlers:
             fmtStr = ('[%(name)-15s %(threadName)-10s %(levelname)-8s '
                     '%(asctime)s] %(message)s')
-            handler = logging.StreamHandler()
+            if logPath:
+                handler = RotatingFileHandler(
+                        logPath, maxBytes=1024*1024*32, backupCount=10)
+            else:
+                handler = logging.StreamHandler()
             handler.setFormatter(logging.Formatter(fmtStr))
             self.log.addHandler(handler)
 
