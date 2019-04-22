fb83d6dda67e3cdda72a98f9fba22bb79504354a
zhouxianggen <zhouxianggen@gmail.com>
2019-04-22 16:32:10 +0800

    update

--- pyobject/pyobject.py
+++ pyobject/pyobject.py
@@ -5,20 +5,21 @@ from datetime import datetime
 
 
 class PyObject(object):
-    def __init__(self, logLevel=logging.INFO, logPath=''):
-        self.log = logging.getLogger(self.__class__.__name__)
-        self.log.setLevel(logLevel)
+    def __init__(self, log_name='', log_path='', log_level=logging.INFO):
+        self.log = logging.getLogger(log_name or self.__class__.__name__)
+        self.log.setLevel(log_level)
         self.log.propagate = False
         if not self.log.handlers:
-            fmtStr = ('[%(name)-15s %(threadName)-10s %(levelname)-8s '
+            fmt = ('[%(name)-15s %(threadName)-10s %(levelname)-8s '
                     '%(asctime)s] %(message)s')
-            if logPath:
+            if log_path:
                 handler = RotatingFileHandler(
-                        logPath, maxBytes=1024*1024*32, backupCount=10)
+                        log_path, maxBytes=1024*1024*32, backupCount=10)
             else:
                 handler = logging.StreamHandler()
-            handler.setFormatter(logging.Formatter(fmtStr))
+            handler.setFormatter(logging.Formatter(fmt))
             self.log.addHandler(handler)
+
 
     @classmethod
     def took(cls, func):
